182c16d73d016d0794c0a2037bf94f190f63ad24
nagasaisneha <nagasaisneha.b@gmail.com>
2015-12-11 23:21:06 +0530

    added right index
    added right index

---
+++ rightNav.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>25thcraft</title>
+<link rel="stylesheet" type="text/css" href="Resources/css/style.css">
+<link type="text/css" rel="stylesheet"  href="Resources/css/responsive.css" />
+<link type="text/css" rel="stylesheet"  href="Resources/css/bootstrap-theme.min.css" />
+<link rel="stylesheet" type="text/css"
+	href="Resources/css/bootstrap.min.css">
+<link rel="stylesheet"
+	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <style>
+        a:hover{
+            text-decoration: none;
+            color: #09c;
+        }
+        a{
+            color: #000;
+        }
+    </style>
+    </head>
+     <body>
+        <div class="container">
+        <div class=row>
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="sideField"><a href="#">Company</a></div>
+            <div class="sideField"><a href="#">Experience</a></div>
+            <div class="sideField"><a href="#">Gallery</a></div>
+            <div class="sideField"><a href="#">Job Posting</a></div>
+            <div class="sideField"><a href="#">Skill</a></div>
+        </div>
+        </div>
+        </div>
+    
+    </body>
+</html>
--- usernameUpdateForm.php
+++ usernameUpdateForm.php
@@ -1,9 +1,25 @@
 <!DOCTYPE html>
 <head>
-<title></title>
-</head>
-<body>
+<title>25thCraft</title>
+    <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+<link rel="stylesheet" type="text/css" href="Resources/css/style.css">
+<link type="text/css" rel="stylesheet"  href="Resources/css/responsive.css" />
+<link type="text/css" rel="stylesheet"  href="Resources/css/bootstrap-theme.min.css" />
+<link rel="stylesheet" type="text/css"
+	href="Resources/css/bootstrap.min.css">
+<link rel="stylesheet"
+	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<script src="http://maps.googleapis.com/maps/api/js?sensor=false&amp;libraries=places" type="text/javascript"></script>
+ <script src="//code.jquery.com/jquery-1.9.1.js"></script>
+  <script src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js"></script>
+
+ 
+</head>
+<body class="bgimage">
+<div class="topspace"></div>
 		
 		<?php 
         include('externallink.php');
@@ -30,13 +46,56 @@ if(isset($_POST['submit'])){
             
 		$updateobj->updateAll("username","usernameid",$_POST['usernameid'], $valuearry);}?></head>
 <body>
+    <!-- start of navigation bar --> 
+	
+	
+	<nav class="navbar navbar-default container navbar-inverse">
+		<div class="container-fluid">
+		
+		
+			<div class="navbar-header pull-left">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#mainnavbar" aria-expanded="false">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        
+      </button>
+      </div>
+     
+      
+    
+			
+			
+				<div class="collapse navbar-collapse" id="mainnavbar" aria-expanded="false">
+				<ul class="nav navbar-nav">
+					<li><a data-toggle="pill" href="Alljobs.html">All Jobs</a></li>
+					<li><a data-toggle="pill" href="iitmjobs.html">IIT/IIM Jobs</a></li>
+					<li><a data-toggle="pill" href="govtjobs.html">Govt. Jobs</a></li>
+					<li><a data-toggle="pill" href="intjobs.html">International Jobs</a></li>
+				</ul>
+				</div>
+			</div>
+		
+	</nav>
+	
+	
+<!-- end of navigation bar --> 
+    <div class="container">
+<div class="row">
+    <div  class="col-md-3 col-sm-12 col-xs-12" style="margin-top:20px;">
+        <div class="right-nav right-nav-sp refine">
+         <?php include("rightNav.php"); ?>
+        </div>
+    </div>
+    
+    <div class="col-md-6 col-sm-12 col-xs-12 xspt bodyFont">
+        <div class="middle-content">
+				
 
-		<form action="usernameupdateForm.php" method="post" class='form-horizontal'>
+        <form action="usernameupdateForm.php" method="post" class='form-horizontal' id="userUpdate-form" novalidate="novalidate">
 
-
-<div class='container'>
-  <h2 class='text-capitalize'>username Update Form</h2>
- <div class='form-group'>
+  <h2 class='text-capitalize userForm'>username Update Form</h2>
+ <div class='form-group userM'>
 
  
       <input type='hidden' class='form-control' id='usernameid' placeholder='usernameid' value='<?php echo $individualDetails[0]['usernameid'] ?>'>
@@ -49,52 +108,52 @@ if(isset($_POST['submit'])){
 
  
 
-          <div class='form-group'>
+          <div class='form-group userM'>
 
-    <label class='control-label col-sm-2 col-md-3 text-capitalize' for='fullName' required >fullName:</label>
+    <label class='control-label col-sm-2 col-md-4 col-xs-12 text-capitalize' for='fullName' required >Full Name:</label>
 
-    <div class='col-sm-10 col-md-5'>
+    <div class='col-sm-10 col-md-6 col-xs-12'>
 
-      <input type='text' class='form-control' id='fullName' placeholder='fullName' value='<?php echo $individualDetails[0]['fullName'] ?>'>
-
-    </div>
-
-  </div>
-
-  
-    <div class='form-group'>
-
-    <label class='control-label col-sm-2 col-md-3 text-capitalize' for='email'>email:</label>
-
-    <div class='col-sm-10 col-md-5'>
-
-      <input type='text' class='form-control' id='email' placeholder='email' value='<?php echo $individualDetails[0]['email'] ?>'>
+      <input type='text' class='form-control' id='fullName' placeholder='fullName'  value='<?php echo $individualDetails[0]['fullName'] ?>'>
 
     </div>
 
   </div>
 
   
-    <div class='form-group'>
+    <div class='form-group userM'>
 
-    <label class='control-label col-sm-2 col-md-3 text-capitalize' for='dob'>dob:</label>
+    <label class='control-label col-sm-2 col-md-4 text-capitalize' for='email'>Email:</label>
 
-    <div class='col-sm-10 col-md-5'>
-
-      <input type='text' class='form-control' id='dob' placeholder='dob' value='<?php echo $individualDetails[0]['dob'] ?>'>
+    <div class='col-sm-10 col-md-6'>
+	
+      <input type='text' class='form-control' id='email' placeholder='email'  value='<?php echo $individualDetails[0]['email'] ?>'>
 
     </div>
 
   </div>
 
   
-    <div class='form-group'>
+    <div class='form-group userM'>
 
-    <label class='control-label col-sm-2 col-md-3 text-capitalize' for='password'>password:</label>
+    <label class='control-label col-sm-2 col-md-4 text-capitalize' for='dob'>DOB:</label>
 
-    <div class='col-sm-10 col-md-5'>
+    <div class='col-sm-10 col-md-6'>
+		
+      <input type='text' class='form-control' id='dob'  placeholder='dob' value='<?php echo $individualDetails[0]['dob'] ?>'>
 
-      <input type='password' class='form-control' id='password' placeholder='password' value='<?php echo $individualDetails[0]['password'] ?>'>
+    </div>
+
+  </div>
+
+  
+    <div class='form-group userM'>
+
+    <label class='control-label col-sm-2 col-md-4 text-capitalize' for='password'>password:</label>
+
+    <div class='col-sm-10 col-md-6'>
+
+      <input type='password' class='form-control'  id='password' placeholder='password' value='<?php echo $individualDetails[0]['password'] ?>'>
 
     </div>
 
@@ -111,37 +170,37 @@ if(isset($_POST['submit'])){
 
 
   
-    <div class='form-group'>
+    <div class='form-group userM'>
 
-    <label class='control-label col-sm-2 col-md-3 text-capitalize' for='fullAddress'>fullAddress:</label>
+    <label class='control-label col-sm-2 col-md-4 text-capitalize' for='fullAddress'>full Address:</label>
 
-    <div class='col-sm-10 col-md-5'>
+    <div class='col-sm-10 col-md-6'>
 
-      <input type='text' class='form-control' id='fullAddress' placeholder='fullAddress' value='<?php echo $individualDetails[0]['fullAddress'] ?>'>
-
-    </div>
-
-  </div>
-
-  
-    <div class='form-group'>
-
-    <label class='control-label col-sm-2 col-md-3 text-capitalize' for='city'>city:</label>
-
-    <div class='col-sm-10 col-md-5'>
-
-      <input type='text' class='form-control' id='city' placeholder='city' value='<?php echo $individualDetails[0]['city'] ?>'>
+      <input type='text' class='form-control'  id='fullAddress' placeholder='fullAddress' value='<?php echo $individualDetails[0]['fullAddress'] ?>'>
 
     </div>
 
   </div>
 
   
-    <div class='form-group'>
+    <div class='form-group userM'>
 
-    <label class='control-label col-sm-2 col-md-3 text-capitalize' for='phoneNumber'>phoneNumber:</label>
+    <label class='control-label col-sm-2 col-md-4 text-capitalize' for='city'>city:</label>
 
-    <div class='col-sm-10 col-md-5'>
+    <div class='col-sm-10 col-md-6'>
+
+      <input type='text' class='form-control'  id='city' placeholder='city' value='<?php echo $individualDetails[0]['city'] ?>'>
+
+    </div>
+
+  </div>
+
+  
+    <div class='form-group userM'>
+
+    <label class='control-label col-sm-2 col-md-4 text-capitalize' for='phoneNumber'>phone Number:</label>
+
+    <div class='col-sm-10 col-md-6'>
 
       <input type='text' class='form-control' id='phoneNumber' placeholder='phoneNumber' value='<?php echo $individualDetails[0]['phoneNumber'] ?>'>
 
@@ -150,24 +209,24 @@ if(isset($_POST['submit'])){
   </div>
 
   
-    <div class='form-group'>
+    <div class='form-group userM'>
 
-    <label class='control-label col-sm-2 col-md-3 text-capitalize' for='personalWebsite'>personalWebsite:</label>
+    <label class='control-label col-sm-2 col-md-4 text-capitalize' for='personalWebsite'>personal Website:</label>
 
-    <div class='col-sm-10 col-md-5'>
+    <div class='col-sm-10 col-md-6'>
 
-      <input type='text' class='form-control' id='personalWebsite' placeholder='personalWebsite' value='<?php echo $individualDetails[0]['personalWebsite'] ?>'>
+      <input type='text' class='form-control'  id='personalWebsite' placeholder='personalWebsite' value='<?php echo $individualDetails[0]['personalWebsite'] ?>'>
 
     </div>
 
   </div>
 
   
-    <div class='form-group'>
+    <div class='form-group userM'>
 
-    <label class='control-label col-sm-2 col-md-3 text-capitalize' for='languages'>languages:</label>
+    <label class='control-label col-sm-2 col-md-4 text-capitalize' for='languages'>languages:</label>
 
-    <div class='col-sm-10 col-md-5'>
+    <div class='col-sm-10 col-md-6'>
 
       <input type='text' class='form-control' id='languages' placeholder='languages' value='<?php echo $individualDetails[0]['languages'] ?>'>
 
@@ -176,13 +235,13 @@ if(isset($_POST['submit'])){
   </div>
 
   
-    <div class='form-group'>
+    <div class='form-group userM'>
 
-    <label class='control-label col-sm-2 col-md-3 text-capitalize' for='insearchofjob'>insearchofjob:</label>
+    <label class='control-label col-sm-2 col-md-4 text-capitalize' for='insearchofjob'>insearch of job:</label>
 
-    <div class='col-sm-10 col-md-5'>
+    <div class='col-sm-10 col-md-6'>
 
-      <input type='radio' name="insearchofjob" id='insearchofjob' placeholder='insearchofjob'<?php if($individualDetails[0]['insearchofjob'] == '1'){echo "checked";} ?> value='1'>Yes
+      <input type='radio' name="insearchofjob" id='insearchofjob' placeholder='insearchofjob' checked <?php if($individualDetails[0]['insearchofjob'] == '1'){echo "checked";} ?> value='1'>Yes
       <input type='radio' name="insearchofjob" id='insearchofjob' placeholder='insearchofjob' <?php if($individualDetails[0]['insearchofjob'] == '0'){echo "checked";} ?> value='0'>No
 
     </div>
@@ -212,19 +271,23 @@ if(isset($_POST['submit'])){
   
 
  
- <div class='form-group'>      
+ <div class='form-group userM'>      
   
       <div class='col-sm-offset-2 col-sm-10'>
 
-        <button type='submit' class='btn btn-default' id='usernamesubmit' >Submit</button>
+        <button type='submit' class='btn btn-primary' id='usernamesubmit' >Submit</button>
 
       </div>
 
     </div>
 
 </div>
-
-                </form>
+     </form>
+    
+    </div>
+    </div>
+    </div>
+    </div>
 
 		<script>
 $(document).ready(function(){
@@ -276,7 +339,66 @@ submit:'submit',
 			alert('succsessfully Updated'+data);
 		
 		});  }); });
+		
+		
+		 $(function() {
+			  $("#userUpdate-form").validate({
+    
+        // Specify the validation rules
+        rules: {
+            fullName: "required",
+            email: {
+                required: true,
+                email: true
+            },
+			dob:"required",
+            password: {
+                required: true,
+                minlength: 5
+            },
+            fullAddress:"required",
+			city:"required",
+			phoneNumber:"required",
+			languages:"required",
+			
+        },
+        
+        // Specify the validation error messages
+        messages: {
+            fullName: "Please enter your full name",
+            email: "Please enter a valid email address",
+			dob: "please enter your date of birth",
+            password: {
+                required: "Please provide a password",
+                minlength: "Your password must be at least 6 characters long"
+            },
+            fullAddress: "please enter your address",
+			city: "please enter your city",
+			phoneNumber: "please enter your phone number",
+			
+        },
+        
+        submitHandler: function(form) {
+            form.submit();
+        }
+    });
 
-  </script>
+  });
+		
+		
+		
+		
+
+ function initialize() {
+  var options = {
+  types: ['(cities)'],
+  componentRestrictions: {country: "in"}
+ };
+
+ var input = document.getElementById('city');
+ var autocomplete = new google.maps.places.Autocomplete(input, options);
+   }
+   google.maps.event.addDomListener(window, 'load', initialize);
+</script>
 		</body>
 </html>
